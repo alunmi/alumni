9d00700924196a132e653c76d9519ae39bda999e
imchukwu <imchukwu@gmail.com>
2018-05-25 18:09:14 +0100

    Empty XML views for the projects

---
+++ app/src/main/java/com/alunmi/alunmi/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.alunmi.alunmi;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initView();
+        initModel();
+    }
+
+    private void initView() {
+    }
+
+    private void initModel() {
+    }
+
+}
